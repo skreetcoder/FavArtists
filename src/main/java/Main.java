f16b960b7bc000073f724a6c1229bc2c077bc8c7
skreetcoder <skreetcoder@gmail.com>
2021-03-12 11:24:06 -0500

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String topThree[] = {"Big Krit", "Ralo", "Nipsey Hussle"};
+        System.out.println(topThree[0]);
+
+
+
+
+
+
+
+
+
+    }
+}
